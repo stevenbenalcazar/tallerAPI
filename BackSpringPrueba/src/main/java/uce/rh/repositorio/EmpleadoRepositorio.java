c31619ab64fb1e256a09f156a65529910408706f
ShanderSandoval <135709230+ShanderSandoval@users.noreply.github.com>
2024-06-26 17:53:23 -0500

    Delete BackSpringPrueba directory

--- BackSpringPrueba/src/main/java/uce/rh/repositorio/EmpleadoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package uce.rh.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uce.rh.modelo.Empleado;
-
-public interface EmpleadoRepositorio extends JpaRepository<Empleado,Integer> {
-}
